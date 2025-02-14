e078a3d8fcaa76ec322109300974bbb0e7275556
Isabelle <bellbebeca@hotmail.com>
2025-02-13 23:08:43 -0300

    Update projetofinal.c

--- projetofinal.c
+++ projetofinal.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-#include <ctype.h>
 #include "pico/stdlib.h"
-#include "pico/binary_info.h"
 #include "inc/ssd1306.h"
 #include "hardware/i2c.h"
 #include "hardware/gpio.h"
